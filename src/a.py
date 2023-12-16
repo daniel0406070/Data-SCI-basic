3c12536811935531e8bbcfb3be0551d15b8b298f
daniel040607 <daniel0406070@gmail.com>
2023-12-16 23:51:49 +0900

    Final

--- src/a.py
+++ src/a.py
@@ -1,27 +1,11 @@
-import cv2
-import tensorflow as tf
-
-# 이미지 불러오기
-img = cv2.imread('image.jpg')
-
-# 이미지 전처리
-img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-img_gray = cv2.GaussianBlur(img_gray, (5, 5), 0)
-_, img_thresh = cv2.threshold(img_gray, 100, 255, cv2.THRESH_BINARY_INV)
-
-# 숫자 인식 모델 불러오기
-model = tf.keras.models.load_model('model.h5')
-
-# 이미지에서 숫자 추출
-contours, _ = cv2.findContours(img_thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-for contour in contours:
-    x, y, w, h = cv2.boundingRect(contour)
-    if w < 10 or h < 10:
-        continue
-    number_img = img_thresh[y:y+h, x:x+w]
-    number_img = cv2.resize(number_img, (28, 28))
-    number_img = number_img.reshape((1, 28, 28, 1))
-    number_img = number_img.astype('float32') / 255
-    prediction = model.predict(number_img)
-    number = tf.argmax(prediction, axis=1)
-    print(number.numpy()[0])
+import numpy as np
+# 주어진 샘플 데이터
+sample_data = np.array([1, 2, 3, 4, 5])
+# 부트스트랩 샘플링
+num_bootstrap_samples = 1000
+bootstrap_samples = np.random.choice(sample_data,(num_bootstrap_samples, len(sample_data)), replace=True)
+# 부트스트랩 평균 계산
+bootstrap_means = np.mean(bootstrap_samples, axis=1)
+# 95% 신뢰 구간 계산
+confidence_interval = np.percentile(bootstrap_means, [2.5, 97.5])
+print("95% 신뢰 구간:", confidence_interval)
